59c7973cf36b02a75ff51f114284aef21220eca6
Htet Naing <htetnaing0814@gmail.com>
2022-12-17 01:33:04 +0630

    restrict empty inputs

--- app.js
+++ app.js
@@ -47,38 +47,40 @@ const initiate = (e) => {
     e.preventDefault();
     let word = e.target.innerHTML;
     const keywords = word.substring(0, 2);
-    switch (keywords) {
-      case '/1':
-        word = createEl('h1', word.substring(2), 'Heading 1');
-        break;
-      case '/2':
-        word = createEl('h2', word.substring(2), 'Heading 2');
-        break;
-      case '/3':
-        word = createEl('h3', word.substring(2), 'Heading 3');
-        break;
-      case '/4':
-        word = createEl('h4', word.substring(2), 'Heading 4');
-        break;
-      case '/5':
-        word = createEl('h5', word.substring(2), 'Heading 5');
-        break;
-      case '/6':
-        word = createEl('h6', word.substring(2), 'Heading 6');
-        break;
-      default:
-        word = createEl('p', word, 'Paragraph Tag');
-        break;
+    if (word.substring(2) !== '') {
+      switch (keywords) {
+        case '/1':
+          word = createEl('h1', word.substring(2), 'Heading 1');
+          break;
+        case '/2':
+          word = createEl('h2', word.substring(2), 'Heading 2');
+          break;
+        case '/3':
+          word = createEl('h3', word.substring(2), 'Heading 3');
+          break;
+        case '/4':
+          word = createEl('h4', word.substring(2), 'Heading 4');
+          break;
+        case '/5':
+          word = createEl('h5', word.substring(2), 'Heading 5');
+          break;
+        case '/6':
+          word = createEl('h6', word.substring(2), 'Heading 6');
+          break;
+        default:
+          word = createEl('p', word, 'Paragraph Tag');
+          break;
+      }
+      root.replaceChild(word, e.target);
+      root.children[root.children.length - 1].focus();
+      const last = root.children[root.children.length - 1];
+      let decision = e.target.innerHTML;
+      const cmd = e.target.innerHTML.substring(0, 2);
+      if (cmd === '/1' || cmd === '/2' || cmd === '/3' || cmd === '/4' || cmd === '/5' || cmd === '/6') {
+        decision = e.target.innerHTML.substring(2);
+      }
+      decision === last.innerHTML && newInput();
     }
-    root.replaceChild(word, e.target);
-    root.children[root.children.length - 1].focus();
-    const last = root.children[root.children.length - 1];
-    let decision = e.target.innerHTML;
-    const cmd = e.target.innerHTML.substring(0, 2);
-    if (cmd === '/1' || cmd === '/2' || cmd === '/3' || cmd === '/4' || cmd === '/5' || cmd === '/6') {
-      decision = e.target.innerHTML.substring(2);
-    }
-    decision === last.innerHTML && newInput();
   } else if (e.key === 'Backspace') {
     e.target.innerHTML === '' && root.children.length > 1 && root.removeChild(e.target);
     root.children[root.children.length - 1].classList.add('active');
